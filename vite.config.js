b523adac31e1381d215a80339f37a1fb972f69e7
Rushnett <9059594+Rushnett@users.noreply.github.com>
2022-08-23 22:03:41 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-]
+	base: process.env.NODE_ENV === "production" ? "/mpi-vis/" : "/",
+	plugins: [
+			vue(),
+			vuetify({ autoImport: true }),
+	]
 })
